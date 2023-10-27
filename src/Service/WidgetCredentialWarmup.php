d8f7a3a9481bd755a38f8518b9916f3677b37b06
sebastien <2le@2le.net>
2023-10-27 10:46:19 +0200

    warmup Widgets

---
+++ src/Service/WidgetCredentialWarmup.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Lle\CruditPlatformBundle\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Lle\CredentialBundle\Contracts\CredentialWarmupInterface;
+use Lle\CredentialBundle\Repository\CredentialRepository;
+use Lle\CredentialBundle\Service\CredentialWarmupTrait;
+use Lle\CruditBundle\Registry\MenuRegistry;
+use Lle\DashboardBundle\Service\WidgetProvider;
+
+class WidgetCredentialWarmup implements CredentialWarmupInterface
+{
+    use CredentialWarmupTrait;
+
+    public function __construct(
+        protected WidgetProvider $widgetProvider,
+        protected CredentialRepository $credentialRepository,
+        protected EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    public function warmUp(): void
+    {
+        $rubrique = "Widgets";
+        $i = 0;
+        foreach ($this->widgetProvider->getWidgetTypes() as $widget) {
+            $this->checkAndCreateCredential(
+                $widget->getRole(),
+                $rubrique,
+                "Widget " . str_replace("ROLE_", "", $widget->getRole()),
+                $i++
+            );
+        }
+    }
+}
